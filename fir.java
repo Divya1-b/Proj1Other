e82013a508735f3c13b87f46f3fe2c812c9bf259
Divya1-b <124667091+Divya1-b@users.noreply.github.com>
2025-10-16 05:18:00 +0000

    First Commit

---
+++ fir.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class fir {
+    public static void main(String[] args) {
+        double sum = 0.0;
+
+        if (args.length > 0) {
+            for (String a : args) {
+                try {
+                    sum += Double.parseDouble(a);
+                } catch (NumberFormatException e) {
+                    System.err.println("Skipping non-number argument: " + a);
+                }
+            }
+        } else {
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                String token = scanner.next();
+                try {
+                    sum += Double.parseDouble(token);
+                } catch (NumberFormatException e) {
+                    // ignore non-numeric tokens
+                }
+            }
+            scanner.close();
+        }
+
+        System.out.println(sum);
+    }
+}public static
